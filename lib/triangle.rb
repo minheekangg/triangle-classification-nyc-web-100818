3fb252b48bc77020cfe505c7ea2e69d66cf00e3c
Min Hee  Park <minheekangg@gmail.com>
2018-10-17 21:31:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
 
 
   def kind
-    if (s1 + s2) > s3 || (s2 + s3) > s1 || (s3 + s1) > s2 || s1 <=0 || s2 <= 0 || s3 <=0
+    if (s1 + s2) < s3 || (s2 + s3) < s1 || (s3 + s1) < s2 || s1 <=0 || s2 <= 0 || s3 <=0
           begin
             raise TriangleError
           end
