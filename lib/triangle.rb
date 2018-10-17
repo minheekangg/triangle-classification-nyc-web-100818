872a5a9c89866b8c3bf9aaa078e1db6e32a54fe8
Min Hee  Park <minheekangg@gmail.com>
2018-10-17 21:27:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if (s1 + s2) > s3 || (s2 + s3) > s1 || (s3 + s1) > s2 || s1 <=0 || s2 <= 0 || s3 <=0
         begin
             raise TriangleError
-          rescue TriangleError => error
+          rescue TriangleError
           end
     elsif s1 == s2 && s2 == s3
       return :equilateral
