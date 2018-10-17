95d5c04c2269220de55f7328ab23f43b52bd8ca8
Min Hee  Park <minheekangg@gmail.com>
2018-10-17 20:42:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+
+  def initialize(*side)
+    @side = side
+  end
+
+  def kind
+    
+ #    self.partner = person
+ #   if person.class != Person
+ #     begin
+ #       raise PartnerError
+ #     rescue PartnerError => error
+ #         puts error.message
+ #     end
+ #   else
+ #     person.partner = self
+ #   end
+ # end
+  end
+
+
+  class TriangleError < StandardError
+  
+  end
+
 end
