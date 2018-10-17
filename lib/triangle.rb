e82ffd5c4114d4167c9d00aab81c806299be63ef
Min Hee  Park <minheekangg@gmail.com>
2018-10-17 21:30:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,10 +10,10 @@ class Triangle
 
   def kind
     if (s1 + s2) > s3 || (s2 + s3) > s1 || (s3 + s1) > s2 || s1 <=0 || s2 <= 0 || s3 <=0
-          # begin
+          begin
             raise TriangleError
 
-          # end
+          end
     elsif s1 == s2 && s2 == s3
       return :equilateral
     elsif s1 == s2 || s2 == s3 || s3 == s1
