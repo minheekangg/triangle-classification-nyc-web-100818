7cc40598f7dfe094ec316d1a4536bc19d9779d0f
Min Hee  Park <minheekangg@gmail.com>
2018-10-17 21:28:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,10 +9,11 @@ class Triangle
 
 
   def kind
-    if (s1 + s2) > s3 || (s2 + s3) > s1 || (s3 + s1) > s2 || s1 <=0 || s2 <= 0 || s3 <=0
-        begin
+    if s1 + s2 >= s3 || s2 + s3 >= s1 || s3 + s1 >= s2 || s1 <=0 || s2 <= 0 || s3 <=0
+          begin
             raise TriangleError
-          rescue TriangleError
+          # rescue TriangleError => error
+          #   puts error.message
           end
     elsif s1 == s2 && s2 == s3
       return :equilateral
@@ -23,11 +24,22 @@ class Triangle
     end
   end
 
+ #    self.partner = person
+ #   if person.class != Person
+ #     begin
+ #       raise PartnerError
+ #     rescue PartnerError => error
+ #         puts error.message
+ #     end
+ #   else
+ #     person.partner = self
+ #   end
+ # end
+
 
 class TriangleError < StandardError
-  def message
-    "error"
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
   end
  end
-
 end
